01ab5cf557b492e866802ec5353b3b5ea99d1027
JeongSooHyeon <jsh75721@naver.com>
2023-11-28 23:44:41 +0900

    land 애니메이션 중

--- Assets/1_Scripts/PlayerControl.cs
+++ Assets/1_Scripts/PlayerControl.cs
@@ -6,33 +6,74 @@ public class PlayerControl : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float rSpeed;
+    [SerializeField] float jPower;
+
     float hAxis;
     float vAxis;
-
     bool rDown;
+    bool jDown;
+
+    bool isJump;
 
     Vector3 moveVec;
 
+    Rigidbody rigid;
     Animator anim;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         rDown = Input.GetButton("Run");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (rDown ? rSpeed : 1f) * Time.deltaTime;
 
         anim.SetBool("isWalk", moveVec != Vector3.zero);
         anim.SetBool("isRun", rDown);
+    }
 
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && !isJump)
+        {
+            rigid.AddForce(Vector3.up * jPower, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
     }
 }
